e18944b5085ff0b6d40c5ed940c4d0e0506b32d4
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-07-25 23:51:15 -0300

    :sparkles: i18 translation

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import Deshboard from './Pages/Deshboard';
+import AppLoading from 'expo-app-loading';
+
+import {
+  useFonts,
+  Mulish_400Regular,
+  Mulish_500Medium,
+  Mulish_600SemiBold,
+  Mulish_700Bold,
+  Mulish_900Black,
+} from '@expo-google-fonts/mulish';
+
 
 export default function App() {
+  let [fontsLoaded, error] = useFonts({
+    Mulish_400Regular,
+    Mulish_500Medium,
+    Mulish_700Bold,
+    Mulish_600SemiBold,
+    Mulish_900Black,
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (<View style={styles.container}>
     <Deshboard />
     <StatusBar style="auto" />
