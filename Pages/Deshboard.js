e4b7ba45023d182bd0c6f98a28d2effdd5f2970d
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-07-25 23:54:20 -0300

    :hammer: Adjustments in dashboard

--- Pages/Deshboard.js
+++ Pages/Deshboard.js
@@ -1,23 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import WeatherInfo from '../Components/WeatherInfo';
+import UnitsPicker from '../Components/UnitsPicker';
+import { colors } from '../Utils'
+import ReloadIcon from '../Components/ReloadIcon';
+import WeatherDetails from '../Components/WeatherDetails';
+import getCountryName from '../Utils/ConvertToNameCountry';
+import * as Localization from 'expo-localization';
 import * as Location from 'expo-location'
-import api from '../Services/api';
+import List5days from '../Components/List5days';
 
 const WEATHER_API_KEY = 'b3dd48c52ebbab270398c3c1747dc11a'
-const DATA_LANG = 'pt_br'
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+const BASE_URL2 = (lat, long, unitySystem) => `https://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${long}&cnt=5&appid=${WEATHER_API_KEY}`
 
 const Deshboard = () => {
     const [errorMessage, setErrorMessage] = useState(null)
     const [currentWeather, setCurrentWeather] = useState(null)
     const [unitySystem, setUnitySystem] = useState('metric')
+    const [fiveDaysData, setFiveDaysData] = useState(null)
 
     useEffect(() => {
         load()
-    }, [])
+    }, [unitySystem])
 
     async function load() {
+        setCurrentWeather(null)
+        setErrorMessage(null)
+        setFiveDaysData(null)
+
         try {
             let { status } = await Location.requestForegroundPermissionsAsync();
 
@@ -29,14 +41,20 @@ const Deshboard = () => {
 
             const { latitude, longitude } = location.coords;
 
-            const weatherUrl = `${BASE_URL}lat=${latitude}&lon=${longitude}&units=${unitySystem}&appid=${WEATHER_API_KEY}&lang=${DATA_LANG}`
+            const weatherUrl = `${BASE_URL}lat=${latitude}&lon=${longitude}&units=${unitySystem}&appid=${WEATHER_API_KEY}&lang=${Localization.locale}`
 
+            
             const response = await fetch(weatherUrl)
+            
+            const fiveDaysResponse = await fetch(BASE_URL2(latitude, longitude))
 
             const result = await response.json()
 
-            if (response.ok) {
+            const fiveDaysResult = await fiveDaysResponse.json()
+
+            if (response.ok && fiveDaysResponse.ok) {
                 setCurrentWeather(result)
+                setFiveDaysData(fiveDaysResult)
             } else {
                 setErrorMessage(result.message)
             }
@@ -45,20 +63,34 @@ const Deshboard = () => {
         }
     }
 
-    if (currentWeather) {
-        const { main: { temp }, } = currentWeather
-
+    if (currentWeather && fiveDaysData) {
+        const { name, sys: { country } } = currentWeather;
+        const countryName = getCountryName(country);
         return (
             <View style={styles.container}>
                 <StatusBar style="auto" />
                 <View style={styles.main}>
-                    <Text>{temp}</Text>
+                    <UnitsPicker unitySystem={unitySystem} setUnitySystem={setUnitySystem} />
+                    <ReloadIcon load={load} />
+                    <View style={styles.content}>
+                        <Text style={styles.name}>{name}, {countryName}</Text>
+                        <View style={styles.board}>
+                            <WeatherInfo currentWeather={currentWeather} unitySystem={unitySystem} />
+                            <WeatherDetails currentWeather={currentWeather} unitySystem={unitySystem} />
+                        </View>
+                    </View>
                 </View>
             </View>
         );
+    } else if (errorMessage) {
+        return (<View style={styles.container}>
+            <ReloadIcon load={load} />
+            <Text style={{ textAlign: 'center' }}>{errorMessage}</Text>
+            <StatusBar style="auto" />
+        </View>)
     } else {
         return (<View style={styles.container}>
-            <Text>{errorMessage}</Text>
+            <ActivityIndicator size="large" color={colors.PRIMARY_COLOR} />
             <StatusBar style="auto" />
         </View>)
     }
@@ -72,7 +104,32 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     main: {
+        alignItems: 'center',
         justifyContent: 'center',
         flex: 1
+    },
+    board: {
+        paddingHorizontal: 85,
+        paddingTop: 30,
+        backgroundColor: colors.BOARD_COLOR,
+        borderTopEndRadius: 15,
+        borderTopStartRadius: 15,
+        borderBottomEndRadius: 15,
+        borderBottomStartRadius: 15,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    name: {
+        fontSize: 22,
+        marginBottom: 25,
+        textTransform: 'capitalize',
+        fontFamily: 'Mulish_700Bold',
+        color: colors.PRIMARY_COLOR,
+        textAlign: 'center'
+    },
+    content: {
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        marginHorizontal: 0
     }
 });
