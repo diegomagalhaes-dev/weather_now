8de813d1982bbd13ddcd542368fc5c34b199cd0b
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-07-25 23:56:45 -0300

    :hammer: adjusments

---
+++ Components/List5days.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const List5days = ({ fiveDaysData }) => {
+
+    return (<View><Text></Text></View>)
+}
+
+export default List5days
+
+const styles = StyleSheet.create({})
---
+++ Utils/index.js
@@ -0,0 +1,9 @@
+export const colors = {
+    BOARD_COLOR: '#437afc',
+    DETAIS_COLOR: '#FDC030',
+    DEFAULT_COLOR: '#FEFFFF',   
+    PRIMARY_COLOR: '#303030',
+    SECONDARY_COLOR: '#fa9b14',
+    BORDER_COLOR: '#161763'
+}
+
